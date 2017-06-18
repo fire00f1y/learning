093ca7844a5ede3857150fd75580ef0d5591b3ac
Bryan Davis <fire00f1y@gmail.com>
2017-06-18 03:18:21 -0400

    Finished message packet structure and serialization

--- learn.go
+++ learn.go
@@ -4,60 +4,81 @@ import (
 	"net"
 	"fmt"
 	"strconv"
-	"time"
 	"os"
+	"github.com/fire00f1y/learning/message"
+	"strings"
+	"bufio"
 )
 
 func main() {
-	StartUdpServer()
-	//RunUdpClient()
+	go StartUdpServer()
+	RunUdpClient()
 }
 
 func RunUdpClient() {
-	user := "Client"
+	user := "fire00f1y"
 	serverAddr, err := net.ResolveUDPAddr("udp", "192.168.1.68:37701")
 	if err != nil {
-		fmt.Fprintf(os.Stderr,"[%s] Error while getting UDP Address: %+v\n", user, err)
+		fmt.Fprintf(os.Stderr,"[Client] Error while getting UDP Address: %+v\n", err)
 		os.Exit(-1)
 	} else {
-		fmt.Printf("Successfully resolved server address: %q\n", serverAddr)
+		fmt.Printf("[Client] Successfully resolved server address: %q\n", serverAddr)
 	}
 
 	conn, err := net.DialUDP("udp", nil, serverAddr)
 	if err != nil {
-		fmt.Fprintf(os.Stderr,"[%s] Error while dialing server: %+v\n", user, err)
+		fmt.Fprintf(os.Stderr,"[Client] Error while dialing server: %+v\n", err)
 		os.Exit(-3)
 	} else {
-		fmt.Printf("Successfully dialed connection to server. Connection: [remote: %q, local: %q]\n", conn.RemoteAddr(), conn.LocalAddr())
+		fmt.Printf("[Client] Successfully dialed connection to server. Connection: [remote: %q, local: %q]\n", conn.RemoteAddr(), conn.LocalAddr())
 	}
+	index := strings.LastIndex(conn.LocalAddr().String(), ":")
+	port := conn.LocalAddr().String()[index+1:]
 	defer conn.Close()
 
+	reader := bufio.NewReader(os.Stdin)
 	for i:=0; i<10000; i++ {
-		msg := "Message number " + strconv.Itoa(i)
-		buf := []byte(msg)
-		fmt.Printf("Sending message: [%s]\n", msg)
+		fmt.Print(".")
+		packet := message.Packet{}
+		packet.User = user
+		i, err = strconv.Atoi(port)
+		if err!= nil {
+			packet.Port = 37701
+		} else {
+			packet.Port = uint16(i)
+		}
+		fmt.Print("$ ")
+		s, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading commandline input: %+v\n", err)
+			continue
+		} else {
+			packet.Message = s
+		}
+		buf, err := packet.BinaryMarshaler()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error while marshalling message: %+v\n", err)
+			continue
+		}
 		_,errs := conn.Write(buf)
 		if errs != nil {
-			fmt.Fprintf(os.Stderr,"Error while writing: %+v\n", errs)
-		} else {
-			fmt.Printf("Send packets to server.\n")
+			fmt.Fprintf(os.Stderr,"[Client] Error while writing: %+v\n", errs)
 		}
-		time.Sleep(1000 * time.Millisecond)
 	}
 }
 
 func StartUdpServer() {
 	serverAddr, err := net.ResolveUDPAddr("udp", ":37701")
 	if err != nil {
-		fmt.Fprintf(os.Stderr,"Error while getting UDP Address: %+v\n", err)
+		fmt.Fprintf(os.Stderr,"[Server] Error while getting UDP Address: %+v\n", err)
 	} else {
-		fmt.Printf("Successfully resolved server address: %q\n", serverAddr)
+		fmt.Printf("[Server] Successfully resolved server address: %q\n", serverAddr)
 	}
 	conn, err := net.ListenUDP("udp", serverAddr)
 	if err != nil {
-		fmt.Fprintf(os.Stderr,"Error while listening at UDP Address %q: %+v\n", serverAddr, err)
+		fmt.Fprintf(os.Stderr,"[Server] Error while listening at UDP Address %q: %+v\n", serverAddr, err)
 	} else {
-		fmt.Printf("Successfully started listening at address: %q\n", serverAddr)
+		fmt.Printf("[Server] Successfully started listening at address: %q\n", serverAddr)
 	}
 	defer conn.Close()
 
@@ -66,9 +87,10 @@ func StartUdpServer() {
 		fmt.Println("Awaiting packets...")
 		n, addr, err := conn.ReadFromUDP(buffer)
 		if err != nil {
-			fmt.Fprintf(os.Stderr,"Error while reading UDP buffer: %+v\n", err)
+			fmt.Fprintf(os.Stderr,"[Server] Error while reading UDP buffer: %+v\n", err)
 		} else {
-			fmt.Printf("[%s] %s\n", addr.IP.String(), string(buffer[0:n]))
+			packet := message.New(buffer[0:n])
+			fmt.Printf("Message from %q:\n%s\n", addr, packet.Print())
 		}
 	}
 }
--- message/types.go
+++ message/types.go
@@ -6,9 +6,14 @@ import (
 	"fmt"
 	"os"
 	"github.com/fire00f1y/learning/utils"
+	"time"
+	"strings"
 )
 
-var byteOrder binary.ByteOrder
+var (
+	byteOrder  binary.ByteOrder
+	DateFormat = "2006-01-02 15:04:05.000"
+)
 
 func init() {
 	if utils.BigEndian() {
@@ -18,55 +23,110 @@ func init() {
 	}
 }
 
-type Packet struct {
-	User string
-	Message string
-	Port uint16
-}
-
 /*
 Packet byte structure:
-bytes [0:1]: Port value
-byte  [2:2]: Username length
-bytes [3:4]: Message length
-bytes [5:5+${Username length}]: Username
-bytes [5+${Username length}: ]: Message
+byte  [0:0]: Username length
+bytes [1:2]: Port value
+bttes [3:24]: Timestamp
+bytes [24:24+${Username length}]: Username
+bytes [24+${Username length} + 1: ]: Message
  */
-func (p Packet) Stream() ([]byte) {
-	bytes := make([]byte, 0)
+type Packet struct {
+	Port      uint16	`bytes:"2"`
+	User      string	`bytes:"len(.User)"`
+	Timestamp string	`bytes:"23"`
+	Message   string	`bytes:"len(.Message)"`
+}
 
+func (p Packet) BinaryMarshaler() ([]byte, error) {
+	// Create byte arrays
+	portBytes := make([]byte, 2)
+	userLength := make([]byte, 1)
+	timestamp := []byte(time.Now().Format(DateFormat))
 	userBytes := []byte(p.User)
 	mesBytes := []byte(p.Message)
 
-	// Serialize port
+	// Calculate total size and allocate output bytes
+	total := len(userBytes) + len(mesBytes) + len(portBytes) + len(userLength) + len(timestamp)
+	mod := 4 - (total % 4)
+	returnbytes := make([]byte, total+mod)
+	index := 0
+
+	// Populate metadata bytes
 	buffer := new(bytes.Buffer)
-	portBytes := make([]byte, 2)
-	err := binary.Write(buffer, byteOrder, p.Port)
+	err := binary.Write(buffer, binary.LittleEndian, p.Port)
 	if err != nil {
-		a, b := uint8(p.Port>>8), uint8(p.Port&0xff)
-		bytes = append(bytes, a, b)
-		fmt.Fprintf(os.Stderr, "Error converting port %d to LittleEndian byte array: %+v\nConverted via bitshifting: %x\n", p.Port, err, portBytes)
+		fmt.Fprintf(os.Stderr, "Error converting port to binary: %+v\n", err)
+		portBytes[0], portBytes[1] = uint8(p.Port>>8), uint8(p.Port&0xff)
 	} else {
 		portBytes = buffer.Bytes()
-		for _, v := range portBytes {
-			bytes = append(bytes, v)
-		}
 	}
 
 	buffer = new(bytes.Buffer)
-	userSizeBytes := make([]byte, 1)
-	err = binary.Write(buffer, byteOrder, len(p.User))
+	err = binary.Write(buffer, binary.LittleEndian, uint8(len(p.User)))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error writing byte array of username: %+v\n", err)
-		userSizeBytes = make([]byte, 256)
-		for i, v := range userBytes {
-
-		}
+		fmt.Fprintf(os.Stderr, "Error converting user length to binary: %+v\n", err)
+		userLength[0] = uint8(len(p.User) & 0xff)
+	} else {
+		userLength = buffer.Bytes()
 	}
 
-	return bytes
+	// Write bytes out
+	for _, v := range userLength {
+		returnbytes[index] = v
+		index++
+	}
+	for _, v := range portBytes {
+		returnbytes[index] = v
+		index++
+	}
+	for _, v := range timestamp {
+		returnbytes[index] = v
+		index++
+	}
+	for _, v := range userBytes {
+		returnbytes[index] = v
+		index++
+	}
+	for _, v := range mesBytes {
+		returnbytes[index] = v
+		index++
+	}
+	for index < total+mod {
+		returnbytes[index] = ' '
+		index += 1
+	}
+
+	return returnbytes, nil
 }
 
-func (p *Packet) Read(bytes []byte) {
+func New(input []byte) (Packet) {
+	p := Packet{}
+	userLength  := input[0:1]
+	var length uint8
+	err := binary.Read(bytes.NewReader(userLength), binary.LittleEndian, &length)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error while reading user name length: %+v\n", err)
+	}
 
+	portBytes := input[1:3]
+	err = binary.Read(bytes.NewReader(portBytes), binary.LittleEndian, &p.Port)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error while reading in port: %+v\n", err)
+	}
+
+	timestamp := input[3:26]
+	p.Timestamp = string(timestamp)
+
+	username := input[26:(26 + length)]
+	p.User = string(username)
+
+	mes := input[26+length:]
+	mesString := string(mes)
+	p.Message = strings.TrimSpace(mesString)
+	return p
+}
+
+func (p Packet) Print() (string) {
+	return fmt.Sprintf("[%s] %s: %s\n", p.Timestamp, p.User, p.Message)
 }
